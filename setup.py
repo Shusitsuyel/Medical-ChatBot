eac8b46c002129488bda418a57cf28a76ff31af3
shushil suyel <124856053+Shusitsuyel@users.noreply.github.com>
2025-02-28 21:36:21 +0545

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical Care Bot",
+    version = "0.0.1",
+    author = "Shushil Suyel",
+    author_email="shusitchettri3210@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
